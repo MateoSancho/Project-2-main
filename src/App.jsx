f3efcc8ddbc99dc6fa59342aba2204baea1d5a54
Mateo Sancho <mat.sancho.e@gmail.com>
2025-11-14 09:31:58 +0100

    final things

--- src/App.jsx
+++ src/App.jsx
@@ -43,3 +43,10 @@ function App() {
 }
 
 export default App
+
+
+//Add to athletes a Description, social media links
+//Add to brands a Description, web links
+//Add more Data for Brands and Athletes
+//Better Css
+//Add a 404 not found page
